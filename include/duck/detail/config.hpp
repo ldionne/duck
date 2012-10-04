b9e6c503b3a337e23fbffa80e503904b291b5aa9
Louis Dionne <louis.dionne92@gmail.com>
2012-10-04 14:10:56 -0400

    Add a config header to help interoperation with libraries.

---
+++ include/duck/detail/config.hpp
@@ -0,0 +1,19 @@
+/**
+ * This file contains wrappers over constructs that are extern to the library.
+ */
+
+#ifndef DUCK_DETAIL_CONFIG_HPP
+#define DUCK_DETAIL_CONFIG_HPP
+
+#include <iterator>
+
+
+namespace duck {
+namespace detail {
+
+using std::iterator_traits;
+
+} // end namespace detail
+} // end namespace duck
+
+#endif // !DUCK_DETAIL_CONFIG_HPP
